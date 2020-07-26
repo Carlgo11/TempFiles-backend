40e4e44fb47fd9ac5393d866743448002faac478
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-07-26 18:28:23 +0200

    Add ability to configure tmpfiles through env variables

--- src/com/carlgo11/tempfiles/config.php
+++ src/com/carlgo11/tempfiles/config.php
@@ -1,20 +1,14 @@
 <?php
 
-/* NOTE:
- * For storing database credentials I use system environment variables.
- * If you'd prefer to just store the credentials in this document then,
- * replace my variables definitions with the ones I've commented.
- */
-
 return [
 	# File storage directory
-	'file-path' => '/tmp/tempfiles/',
+	'file-path' => getenv('TMP_PATH') ?: '/tmp/tempfiles/',
 	# Allowed formats <n>MB, <n>GB, <n>TB, <n>PB.
-	'max-file-size' => '100MB',
+	'max-file-size' => getenv('TMP_MAX_SIZE') ?: '128MB',
 	# Encryption algorithm to use for encrypting uploads.
-	'Encryption-Method' => 'aes-256-gcm',
+	'Encryption-Method' => getenv('TMP_ENCRYPTION_ALGO') ?: 'aes-256-gcm',
 	# Download URL
-	'download-url' => 'https://d.carlgo11.com/%1$s/?p=%2$s',
+	'download-url' => getenv('TMP_DOWNLOAD_URL') ?: 'https://d.carlgo11.com/%1$s/?p=%2$s',
 	# API Download URL
-	'api-download-url' => 'https://api.tempfiles.download/download/?id=%1$s&p=%2$s'
+	'api-download-url' => getenv('TMP_API_DOWNLOAD_URL') ?: 'https://api.tempfiles.download/download/?id=%1$s&p=%2$s'
 ];
