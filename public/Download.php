77f332468dd1653dbc766f650a4262850e6dc486
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-09-21 01:47:59 +0200

    Change folder structure to work with webdevops/php-nginx's nginx configs

--- public/Download.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace com\carlgo11\tempfiles\api;
-require_once __DIR__ . '/../src/com/carlgo11/tempfiles/autoload.php';
-require_once __DIR__ . '/../src/com/carlgo11/tempfiles/api/Download.php';
-
-new Download(filter_var($_SERVER['REQUEST_METHOD'], FILTER_SANITIZE_STRING));
