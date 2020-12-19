52c81b82ecd84d7ef202c20a8c6de8f2db1cd872
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-12-19 01:47:24 +0100

    Quit on OPTIONS request

--- public/delete/index.php
+++ public/delete/index.php
@@ -4,4 +4,6 @@ namespace com\carlgo11\tempfiles\api;
 require_once __DIR__ . '/../../src/com/carlgo11/tempfiles/autoload.php';
 require_once __DIR__ . '/../../src/com/carlgo11/tempfiles/api/Delete.php';
 
+if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'OPTIONS') die(http_response_code(100)); //Quit on OPTIONS request.
+
 new Delete(filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^(DELETE)$/']]));
