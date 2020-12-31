cb7c47bcbc1d762866fbde6aaadd131952c89ceb
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-12-31 03:40:14 +0100

    Send content-type on OPTIONS request to /delete/

--- public/delete/index.php
+++ public/delete/index.php
@@ -4,6 +4,8 @@ namespace com\carlgo11\tempfiles\api;
 require_once __DIR__ . '/../../src/com/carlgo11/tempfiles/autoload.php';
 require_once __DIR__ . '/../../src/com/carlgo11/tempfiles/api/Delete.php';
 
-if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'OPTIONS') die(http_response_code(202)); //Quit on OPTIONS request.
-
+if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'OPTIONS') {
+	header('Content-Type: application/json; charset=utf-8');
+	http_response_code(202); // Ignore OPTIONS requests.
+}else
 new Delete(filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^(DELETE)$/']]));
