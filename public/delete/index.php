4c203784f8232ca70d7a7454538dedda5db40e73
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-12-31 03:56:19 +0100

    hardcode CORS headers on OPTIONS request

--- public/delete/index.php
+++ public/delete/index.php
@@ -5,7 +5,8 @@ require_once __DIR__ . '/../../src/com/carlgo11/tempfiles/autoload.php';
 require_once __DIR__ . '/../../src/com/carlgo11/tempfiles/api/Delete.php';
 
 if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'OPTIONS') {
-	header('Content-Type: application/json; charset=utf-8');
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Methods: DELETE, OPTIONS');
 	http_response_code(202); // Ignore OPTIONS requests.
 }else
 new Delete(filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^(DELETE)$/']]));
