ea4f873a900f3d2ebae7ca2d485c37102dfb4738
Carlgo11 <carlgo11@users.noreply.github.com>
2021-04-28 20:45:13 +0200

    Add documentation to Encryption.php

--- src/com/carlgo11/tempfiles/Encryption.php
+++ src/com/carlgo11/tempfiles/Encryption.php
@@ -12,10 +12,14 @@ use Exception;
 class Encryption {
 
 	/**
-	 * @param string $input
-	 * @param string $password
-	 * @return array
-	 * @throws Exception
+	 * Encrypts string with encryption algorithm specified in config
+	 *
+	 * @param string $input Input data to encrypt.
+	 * @param string $password Plain text password.
+	 * @return array Array containing encrypted version of $input.
+	 * @throws Exception Throws {@see Exception} in the event of encryption error.
+	 * @since 3.0
+	 * @example encrypt("data", "password");
 	 */
 	public static function encrypt(string $input, string $password): array {
 		global $conf;
@@ -28,22 +32,6 @@ class Encryption {
 			$output[] = implode("&", [base64_encode($encrypted), base64_encode($iv), base64_encode($tag)]);
 		}
 		return $output;
-	}
-
-	/**
-	 * @param array $input
-	 * @param string $password
-	 * @return string
-	 */
-	public static function decrypt(array $input, string $password): string {
-		global $conf;
-		$cipher = $conf['Encryption-Method'];
-		$output = [];
-		foreach ($input as $part) {
-			$part = explode("&", $part);
-			array_push($output, openssl_decrypt(base64_decode($part[0]), $cipher, $password, OPENSSL_RAW_DATA, base64_decode($part[1]), base64_decode($part[2])));
-		}
-		return implode("", $output);
 	}
 
 	/**
@@ -60,6 +48,24 @@ class Encryption {
 	public static function createIV(string $cipher): string {
 		$ivLength = openssl_cipher_iv_length($cipher);
 		return random_bytes($ivLength);
+	}
+
+	/**
+	 *
+	 * @param array $input Encrypted data to decrypt.
+	 * @param string $password Plain text decryption password.
+	 * @return string Returns decrypted data in the form of a clear text string.
+	 * @since 3.0
+	 */
+	public static function decrypt(array $input, string $password): string {
+		global $conf;
+		$cipher = $conf['Encryption-Method'];
+		$output = [];
+		foreach ($input as $part) {
+			$part = explode('&', $part);
+			array_push($output, openssl_decrypt(base64_decode($part[0]), $cipher, $password, OPENSSL_RAW_DATA, base64_decode($part[1]), base64_decode($part[2])));
+		}
+		return implode('', $output);
 	}
 
 }
