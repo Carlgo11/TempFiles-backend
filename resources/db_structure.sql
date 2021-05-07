3690abff15059e47a6238ee7ce9ae19a8acf6115
Carlgo11 <carlgo11@users.noreply.github.com>
2021-05-07 02:06:55 +0200

    Add MySQL structure

---
+++ resources/db_structure.sql
@@ -0,0 +1,49 @@
+-- Create USER
+
+--
+-- Create Database
+--
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+CREATE DATABASE IF NOT EXISTS `tempfiles` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `tempfiles`;
+
+--
+-- Create Table: Main
+--
+CREATE TABLE IF NOT EXISTS `main`
+(
+    `id`      varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `expiry`  varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `views`   tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+    `delpass` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+--
+-- Create Table: Content
+--
+CREATE TABLE IF NOT EXISTS `content`
+(
+    `id`   varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `part` tinyint                                                      NOT NULL,
+    `data` mediumblob                                                   NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+--
+-- Create Table: MetaData
+--
+CREATE TABLE IF NOT EXISTS `metadata`
+(
+    `id`   varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `part` tinyint                                                      NOT NULL,
+    `data` mediumtext                                                   NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
